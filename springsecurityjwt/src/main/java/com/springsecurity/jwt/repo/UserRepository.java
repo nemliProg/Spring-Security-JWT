4adfff606a51c6bb554bfa5c64bc55bdc7343c07
nemliProg <younesbuzz@gmail.com>
2023-01-23 03:14:26 +0100

    add findByEmail to get the user by his email

--- springsecurityjwt/src/main/java/com/springsecurity/jwt/repo/UserRepository.java
+++ springsecurityjwt/src/main/java/com/springsecurity/jwt/repo/UserRepository.java
@@ -3,6 +3,9 @@ package com.springsecurity.jwt.repo;
 import com.springsecurity.jwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
 }
