45e6b4a02cfae3c3ef5099392ae6b2871a949430
nemliProg <younesbuzz@gmail.com>
2023-02-04 23:25:03 +0100

    Implement AuthenticationService with authentication and registration functionality

---
+++ springsecurityjwt/src/main/java/com/springsecurity/jwt/web/auth/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.springsecurity.jwt.web.auth;
+
+
+import com.springsecurity.jwt.entity.User;
+import com.springsecurity.jwt.entity.enums.Role;
+import com.springsecurity.jwt.repo.UserRepository;
+import com.springsecurity.jwt.utils.JwtService;
+import com.springsecurity.jwt.web.auth.dto.AuthenticationRequest;
+import com.springsecurity.jwt.web.auth.dto.AuthenticationResponse;
+import com.springsecurity.jwt.web.auth.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
