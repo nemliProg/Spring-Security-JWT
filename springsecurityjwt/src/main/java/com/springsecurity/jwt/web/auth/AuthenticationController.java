ca6f96c1537a6f93126652e2a99a3d2f362b548e
nemliProg <younesbuzz@gmail.com>
2023-02-04 23:26:49 +0100

    Add AuthenticationController for handling authentication and registration requests

---
+++ springsecurityjwt/src/main/java/com/springsecurity/jwt/web/auth/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.springsecurity.jwt.web.auth;
+
+import com.springsecurity.jwt.web.auth.dto.AuthenticationRequest;
+import com.springsecurity.jwt.web.auth.dto.AuthenticationResponse;
+import com.springsecurity.jwt.web.auth.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+
+}
